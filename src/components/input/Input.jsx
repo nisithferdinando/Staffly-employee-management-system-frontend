8af6641ac0cfe8782b50a6318b372f292427116c
Nisith Ferdinando <nisithsandaru@gmail.com>
2025-10-19 14:56:11 +0530

    custome inputs

---
+++ src/components/input/Input.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Input = ({
+    label,
+    type="text",
+    name,
+    value="",
+    onChange=()=>{},
+    placeholder="",
+    disabled=false,
+    size,
+    required=false,
+
+}) => {
+  return (
+    <div>Input</div>
+  )
+}
+
+export default Input
