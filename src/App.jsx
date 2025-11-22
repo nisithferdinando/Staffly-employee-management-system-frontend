a8bb8264bf8a34b1a1eebd424e7bf6502a6b4c70
Raindu <101939207+Raindu@users.noreply.github.com>
2025-11-22 18:52:37 +0530

    employee login completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AdminLogin from "./pages/hr/AdminLogin";
+import EmployeeLogin from "./pages/employee/EmployeeLogin"
 import {
   BrowserRouter as Router,
   Routes,
@@ -17,6 +18,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<AdminLogin />} />
+          <Route path="/employee/login" element={<EmployeeLogin />} />
 
           <Route path="/" element={<DashboardLayout />}>
             <Route path="hr/dashboard" element={<SamplePage />} />
